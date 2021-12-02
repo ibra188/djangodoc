858e1341af77dd4a7646378941e3d9a72da53fe3
ebrima sonko <ebrimasonko188@gmail.com>
2021-12-02 23:42:33 +0100

    added more views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,18 @@
 import django.shortcuts
 
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("ciao tutti sei nella pagina di index polls.")
+def detail(request, question_id):
+    return HttpResponse("ciao tutti sei nella pagina di index polls %s. " % question_id)
+
+def results(request, question_id):
+    response = "stai guardano il resultato di domanda %s."
+    return HttpResponse(response, question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s. "% question_id)
+    
+            
+
